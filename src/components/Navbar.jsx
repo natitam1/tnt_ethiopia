c19615be051d857783e4865e124087403f3feb8b
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-14 23:50:06 +0300

    Add theme toggler functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import assets from "../assets/assets";
+import ThemeToggleBtn from "./ThemeToggleBtn";
 
 const Navbar = ({ theme, setTheme }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -54,7 +55,8 @@ const Navbar = ({ theme, setTheme }) => {
           Contact Us
         </a>
       </div>
-      <div>
+      <div className="flex items-center gap-2 sm:gap-4">
+        <ThemeToggleBtn theme={theme} setTheme={setTheme} />
         <img
           src={theme === "dark" ? assets.menu_icon_dark : assets.menu_icon}
           className="w-8 sm:hidden"

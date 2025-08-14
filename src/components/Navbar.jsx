560fabd2ba92e90cc03130346d96114a4af32edf
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-14 23:26:54 +0300

    Add sidebar toggle functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import assets from "../assets/assets";
 
 const Navbar = ({ theme, setTheme }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <div className="flex items-center justify-between px-4 sm:px-12 lg:px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70">
       <a href="">
@@ -12,6 +13,11 @@ const Navbar = ({ theme, setTheme }) => {
         />
       </a>
       <div className="text-gray-700 dark:text-white sm:text-sm max-sm:w-60 max-sm:pl-10 max-sm:fixed top-0 bottom-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all">
+        <img
+          src={assets.close_icon}
+          alt=""
+          className="w-5 absolute right-4 top-4 sm:hidden"
+        />
         <a href="#" className="sm:hover:border-b">
           Home
         </a>

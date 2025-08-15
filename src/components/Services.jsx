ad0e3b1febb89da476eec3ddabd7d6d7506a8c09
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-15 23:35:14 +0300

    Set up the services page

---
+++ src/components/Services.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import assets from "../assets/assets";
+
+const Services = () => {
+  const servicesData = [
+    {
+      title: "Advertising",
+      description:
+        "We turn bold ideas into powerful digital solutions that connect, engage...",
+      icon: assets.ads_icon,
+    },
+    {
+      title: "Advertising",
+      description:
+        "We turn bold ideas into powerful digital solutions that connect, engage...",
+      icon: assets.marketing_icon,
+    },
+    {
+      title: "Advertising",
+      description:
+        "We turn bold ideas into powerful digital solutions that connect, engage...",
+      icon: assets.content_icon,
+    },
+    {
+      title: "Advertising",
+      description:
+        "We turn bold ideas into powerful digital solutions that connect, engage...",
+      icon: assets.social_icon,
+    },
+  ];
+  return (
+    <div
+      id="services"
+      className="relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
+    >
+      <img
+        src={assets.bgImage2}
+        className="absolute -top-110 -left-70 -z-1 dark:hidden"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Services;

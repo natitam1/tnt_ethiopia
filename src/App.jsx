31bce6cb89aed945dd029623d1a1654f17b6cc61
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-15 22:43:54 +0300

    Set up group profile picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div className="dark:bg-black relative">
       <Navbar theme={theme} setTheme={setTheme} />
+      <Hero />
     </div>
   );
 };
